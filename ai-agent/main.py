515972a57816c97be34c9b82f3bf81a9174cbff1
Chris Lamb <chris.lamb@autotrader.co.uk>
2026-03-04 13:03:55 +0000

    CH1 L5

--- ai-agent/main.py
+++ ai-agent/main.py
@@ -2,6 +2,7 @@ def main():
     print("Hello from ai-agent!")
     import os
     from dotenv import load_dotenv
+    import argparse
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -12,8 +13,15 @@ def main():
 
     client = genai.Client(api_key=api_key)
 
+    parser = argparse.ArgumentParser(description="Gemini AI")
+    parser.add_argument("user_prompt", type=str, help="Ask a question...")
+    args = parser.parse_args()
+
+
+
+
     response = client.models.generate_content(
-    model='gemini-2.5-flash', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+    model='gemini-2.5-flash', contents= args.user_prompt
     )
     if (response.usage_metadata) == None:
         raise ValueError('No response, potential failed API request')
